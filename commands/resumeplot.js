9a1a157b7cd1bfce7819d7f3f2cc3d82802fbe8c
Zach Van <zachvan13@gmail.com>
2018-01-23 14:53:39 -0500

    added resume

---
+++ commands/resumeplot.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'resumeplot',
+    description: 'How do I ruseme plotting?',
+    execute(message, args) {
+        message.reply(`HOW TO RESUME PLOTTING WITH QBUNDLE'S XPLOTTER
+
+See this handy video:
+https://www.youtube.com/watch?v=-8iKW_O2kNs&feature=youtu.b`);
+    },
+};
