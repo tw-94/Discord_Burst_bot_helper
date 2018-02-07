ad3eca333384ba5f55576a0f7a20656fc54009e5
Zach Van <zachvan13@gmail.com>
2018-02-06 23:22:10 -0500

    Fix to -start (I'm smart)

--- commands/start.js
+++ commands/start.js
@@ -6,7 +6,6 @@ module.exports = {
         .then(msg => console.log(`Deleted message from ${msg.author}`))
         .catch(console.error);
         if(message.mentions.users.size === 0) {
-            let TagedMember = message.guild.member(message.mentions.users.first());
              message.reply(`Here is the proper logic:
 Goto faucet and get a coin: https://faucet.burstpay.net/
 1) Set reward recipient: https://burstneon.com/rewardassignment.html
@@ -16,6 +15,7 @@ Goto faucet and get a coin: https://faucet.burstpay.net/
 In other words, you can mine without setting your name. Don't waste your first coin on that.
 Let setting the reward assignment be yout first transaction.`);
            } else{
+                let TagedMember = message.guild.member(message.mentions.users.first());
                message.channel.send(`@${TagedMember} Here is the proper logic:
 Goto faucet and get a coin: https://faucet.burstpay.net/
 1) Set reward recipient: https://burstneon.com/rewardassignment.html
