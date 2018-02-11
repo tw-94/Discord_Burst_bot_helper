870f3cada65f7341680856dec08b31484d0e9600
Zach Van <zachvan13@gmail.com>
2018-02-11 00:25:28 -0500

    added -price

---
+++ commands/price.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'Price',
+    description: 'Get the current price of burst from coinmarketcap.com',
+    execute(message, args) {
+        //delete the message
+        message.delete()
+        .then(msg => console.log(`Deleted message from ${msg.author}`))
+        .catch(console.error);
+        console.log(args);
+        var getJSON = require('get-json')
+        getJSON('https://api.coinmarketcap.com/v1/ticker/burst/', function(error, response){
+
+        error
+        // undefined
+
+        message.reply(`Usd price:${response.result.price_usd}
+        BTC Price:${response.result.price_btc}`);
+
+    response.ok
+    // => true
+
+})
+        };
+    }
+};
