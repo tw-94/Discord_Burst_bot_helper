20e203ba9f41b0fc9ee5bf056de19e3597c344e2
Zach Van <zachvan13@gmail.com>
2018-02-20 22:40:51 -0500

    Fixed a typo

--- commands/unmute.js
+++ commands/unmute.js
@@ -5,7 +5,7 @@ module.exports = {
         message.delete()
         .then(msg => console.log(`Deleted message from ${msg.author}`))
         .catch(console.error);
-        if(message.member.roles.has("Comander")) {
+        if(message.member.roles.has("Commander")) {
             let mute = message.guild.roles.find("name", "Muted");
             let member = message.mentions.members.first();
             member.removeRole(role).catch(console.error("Errored out"));
