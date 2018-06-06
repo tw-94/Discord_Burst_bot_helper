bb4e79718dedadcb52f176e1f55e8a4bca560ff4
Zach Van <Zach@zachvan.com>
2018-06-06 15:04:56 -0400

    Updated start

--- commands/start.js
+++ commands/start.js
@@ -5,25 +5,19 @@ module.exports = {
         message.delete()
         .then(msg => console.log(`Deleted message from ${msg.author}`))
         .catch(console.error);
+        var text = `Here is the proper logic:
+        Get a coin! From ether this discord server (kindly ask someone) or from a facuet
+        1) Set reward recipient: https://burstneon.com/rewardassignment.html
+            For burstneon.com, recipient is: BURST-YXZW-JH7M-QKR9-9PKBN
+        2) Mine and earn some coins.
+        3) Set your name when you finally have some coins.
+        In other words, you can mine without setting your name. Don't waste your first coin on that.
+        Let setting the reward assignment be yout first transaction.`;
         if(message.mentions.users.size === 0) {
-             message.reply(`Here is the proper logic:
-Goto faucet and get a coin: https://faucet.burstpay.net/
-1) Set reward recipient: https://burstneon.com/rewardassignment.html
-    For burstneon.com, recipient is: BURST-YXZW-JH7M-QKR9-9PKBN
-2) Mine and earn some coins.
-3) Set your name when you finally have some coins.
-In other words, you can mine without setting your name. Don't waste your first coin on that.
-Let setting the reward assignment be yout first transaction.`);
+             message.reply(text);
            } else{
                 let TagedMember = message.guild.member(message.mentions.users.first());
-               message.channel.send(`${TagedMember} Here is the proper logic:
-Goto faucet and get a coin: https://faucet.burstpay.net/
-1) Set reward recipient: https://burstneon.com/rewardassignment.html
-    For burstneon.com, recipient is: BURST-YXZW-JH7M-QKR9-9PKBN
-2) Mine and earn some coins.
-3) Set your name when you finally have some coins.
-In other words, you can mine without setting your name. Don't waste your first coin on that.
-Let setting the reward assignment be yout first transaction.`);
+               message.channel.send(`${TagedMember} ${text}`);
            }
     },
 };
