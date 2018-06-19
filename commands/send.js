f0269fe320ad70b18a81088f0f8bd5e2d7865573
Zach Van Iderstine <zach@zachvan.com>
2018-06-19 19:25:36 -0400

    Updated send

--- commands/send.js
+++ commands/send.js
@@ -12,7 +12,7 @@ module.exports = {
         var request = require('request');
         let miner = message.guild.roles.find("name", "Helpful miner");
         if(message.member.roles.some(r=>["Helpful miner"].includes(r.name))) {
-            var Url = `https://burst-wallet.daprogs.com/burst?requestType=sendMoney&secretPhrase=${burst_key}&recipient=${args[0]}&amountNQT=100000000&feeNQT=100000000&deadline=60`
+            var Url = `https://burst-wallet.daprogs.com/burst?requestType=sendMoney&secretPhrase=${burst_key}&recipient=${args[0]}&amountNQT=2000000&feeNQT=1000000&deadline=60`
             request({
              url:Url,
              method:"POST",
@@ -22,7 +22,7 @@ module.exports = {
                      getJSON('https://explore.burst.cryptoguru.org/api/v1/account/11304873521866505399', function(error, response){
                         var bal = response.data.balance -1 / 100000000
                         console.log(bal)
-                        message.reply(`Sent 1 BURST to ${args[0]}! Please allow up to 10 mins for it to apper before trying again! New facuet bal ${bal}`);
+                        message.reply(`Sent .02 BURST to ${args[0]}! Please allow up to 10 mins for it to apper before trying again! New facuet bal ${bal}`);
                     })
                      if (args[2] == "output"){
                          message.reply(body);
